dc71367d85b49b9929bb5f0398fe49b0e95a24ee
Qing-Cheng Li <qc.linux@gmail.com>
2016-01-21 21:56:27 +0800

    Add unit test

---
+++ WarmHandsTests/WarmHandsTests.swift
@@ -0,0 +1,36 @@
+//
+//  WarmHandsTests.swift
+//  WarmHandsTests
+//
+//  Created by Qing-Cheng Li on 2016/1/21.
+//  Copyright © 2016年 QCLS. All rights reserved.
+//
+
+import XCTest
+@testable import WarmHands
+
+class WarmHandsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
