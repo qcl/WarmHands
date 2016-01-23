f5ba502a7a4e09f6cb978fa12524c0869ddd0c47
Qing-Cheng Li <qc.linux@gmail.com>
2016-01-23 22:06:15 +0800

    Try to use Pods

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'AFNetworking', '~> 3.0'
+
+target 'WarmHands' do
+
+end
+
+target 'WarmHandsTests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-WarmHandsTests/Pods-WarmHandsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WarmHandsTests : NSObject
+@end
+@implementation PodsDummy_Pods_WarmHandsTests
+@end
